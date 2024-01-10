05a3a465e0b5608da6ac24318718ec368ec5a6a4
CK <cklee7755@gmail.com>
2024-01-10 12:32:30 +0800

    combine namespace to default except mongo

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -2,10 +2,16 @@
 
 #set -x
 
-export NAMESPACE_DEPARTMENT=department
-export NAMESPACE_EMPLOYEE=employee
-export NAMESPACE_GATEWAY=gateway
-export NAMESPACE_ORGANIZATION=organization
+# export NAMESPACE_DEPARTMENT=department
+# export NAMESPACE_EMPLOYEE=employee
+# export NAMESPACE_GATEWAY=gateway
+# export NAMESPACE_ORGANIZATION=organization
+# export NAMESPACE_MONGO=mongo
+
+export NAMESPACE_DEPARTMENT=default
+export NAMESPACE_EMPLOYEE=default
+export NAMESPACE_GATEWAY=default
+export NAMESPACE_ORGANIZATION=default
 export NAMESPACE_MONGO=mongo
 
 export SA_NAME=api-service-account
