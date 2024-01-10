187e654b199fb91b885a0ad0b9fd45228e791394
CK <cklee7755@gmail.com>
2024-01-10 12:34:03 +0800

    change namespace to default

--- scripts/gateway-open.sh
+++ scripts/gateway-open.sh
@@ -6,4 +6,4 @@ set -x
 
 #xdg-open $(minikube service gateway --url -n=$NAMESPACE_GATEWAY)/swagger-ui.html
 
-kubectl get pod -n $NAMESPACE_GATEWAY -l 'app=gateway' --no-headers | awk '{print $1}' | xargs -I {} sh -c "echo {}; xdg-open $(minikube service gateway --url -n gateway)/swagger-ui.html"
+kubectl get pod -n $NAMESPACE_GATEWAY -l 'app=gateway' --no-headers | awk '{print $1}' | xargs -I {} sh -c "echo {}; xdg-open $(minikube service gateway --url -n default)/swagger-ui.html"
